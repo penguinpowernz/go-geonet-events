43c32b60a8a0b4bedeceb672015bb2b37c40e432
Robert McLeod <robert@penguinpower.co.nz>
2020-03-15 22:34:41 +1300

    make embed_index.sh include version

--- scripts/embed_index.sh
+++ scripts/embed_index.sh
@@ -5,7 +5,11 @@ html="$(cat index.html)"
 cat <<EOF >index.html.go
 package events
 
+var version string
+
 var indexHTML = []byte(\`$html\`)
 EOF
+
+sed 's/<\/title>/ v\`\+version\+\`<\/title>/' -i index.html.go
 
 gofmt -w index.html.go
